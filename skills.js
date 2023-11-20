8f1e0147265cc33380bc1ff2b873c30bffe2413e
Pacharaco23 <140283653+Pacharaco23@users.noreply.github.com>
2023-11-20 22:15:46 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,7 @@
+function calculateNumbers(var1, var2){
+    // Perform calculations here using var1 and var2
+    let result = var1 + var2; // For example, adding var1 and var2
+    
+    // Return the generated result
+    return result;
+}
